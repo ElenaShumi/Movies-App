47a7071b34d32994b804167b173b052c2063e793
Elena Shumilova <shumilovalena2011@gmail.com>
2024-07-25 21:15:53 +0700

    Movies App #2 - Лоадер и обработка ошибок

--- src/components/app.js
+++ src/components/app.js
@@ -15,12 +15,13 @@ export default class App extends Component {
               style={{
                 margin: '0 auto',
                 width: '1100px',
+                minHeight: '100vh',
               }}
             >
               <div
                 style={{
                   background: '#FFFFFF',
-                  minHeight: 280,
+                  minHeight: '100vh',
                   padding: '21px 7%',
                 }}
               >
--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Flex, Card, Typography, Col } from 'antd'
+import { Flex, Card, Typography, Col, Spin, Alert } from 'antd'
 import { format } from 'date-fns'
 
 import MovieService from '../services/movieService'
@@ -21,10 +21,8 @@ export default class MovieCard extends Component {
   movieService = new MovieService()
 
   state = {
-    title: null,
-    overview: null,
-    releaseDate: null,
     movieList: [],
+    loading: true,
   }
 
   constructor() {
@@ -32,12 +30,23 @@ export default class MovieCard extends Component {
     this.updateMovies()
   }
 
-  updateMovies() {
-    this.movieService.getAllMovies().then((res) => {
-      this.setState({
-        movieList: res,
-      })
+  onMoviesLoaded = (movies) => {
+    this.setState({
+      movieList: movies,
+      loading: false,
+      error: false,
     })
+  }
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    })
+  }
+
+  updateMovies() {
+    this.movieService.getAllMovies().then(this.onMoviesLoaded).catch(this.onError)
   }
 
   truncateOverview(str) {
@@ -45,30 +54,72 @@ export default class MovieCard extends Component {
   }
 
   render() {
-    const { movieList } = this.state
+    const { movieList, loading, error } = this.state
+
+    const hasDate = !(loading || error)
+
+    const errorMessage = error ? <ErrorIndicator /> : null
+    const spinner = loading ? <Spiner /> : null
+    const content = hasDate ? <MoviesView movieList={movieList} /> : null
+
     return (
       <>
-        {movieList.map((movie) => (
-          <Col key={movie.id} span={12}>
-            <Card hoverable style={cardStyle} styles={{ body: { padding: 0, overflow: 'hidden' } }}>
-              <Flex justify="start">
-                <img alt="avatar" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} style={imgStyle} />
-                <Flex vertical style={{ padding: '10px 20px' }}>
-                  <Title level={4}>{movie.title}</Title>
-                  <Paragraph>
-                    {movie.release_date ? format(new Date(movie.release_date), 'MMMM dd, yyyy') : ' '}
-                  </Paragraph>
-                  <Paragraph>
-                    <Text code>Action</Text>
-                    <Text code>Drama</Text>
-                  </Paragraph>
-                  <Paragraph>{this.truncateOverview(movie.overview)}</Paragraph>
-                </Flex>
-              </Flex>
-            </Card>
-          </Col>
-        ))}
+        {errorMessage}
+        {spinner}
+        {content}
       </>
     )
   }
+}
+
+const MoviesView = ({ movieList }) => {
+  let truncateOverview = (str) => {
+    return str.length > 190 ? str.slice(0, str.indexOf(' ', 190)) + '…' : str
+  }
+  return (
+    <>
+      {movieList.map((movie) => (
+        <Col key={movie.id} span={12}>
+          <Card hoverable style={cardStyle} styles={{ body: { padding: 0, overflow: 'hidden' } }}>
+            <Flex justify="start">
+              <img alt="avatar" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} style={imgStyle} />
+              <Flex vertical style={{ padding: '10px 20px' }}>
+                <Title level={4}>{movie.title}</Title>
+                <Paragraph>
+                  {movie.release_date ? format(new Date(movie.release_date), 'MMMM dd, yyyy') : ' '}
+                </Paragraph>
+                <Paragraph>
+                  <Text code>Action</Text>
+                  <Text code>Drama</Text>
+                </Paragraph>
+                <Paragraph>{truncateOverview(movie.overview)}</Paragraph>
+              </Flex>
+            </Flex>
+          </Card>
+        </Col>
+      ))}
+    </>
+  )
+}
+
+const Spiner = () => {
+  return (
+    <>
+      <Spin fullscreen="true" tip="Loading" size="large" />
+    </>
+  )
+}
+
+const ErrorIndicator = () => {
+  return (
+    <>
+      <Alert
+        style={{ margin: '100px auto' }}
+        message="Error"
+        description="Something went wrong. Please try again"
+        type="error"
+        showIcon
+      />
+    </>
+  )
 }
